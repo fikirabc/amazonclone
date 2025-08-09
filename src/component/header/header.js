ef83a1c3e4b5cba765d12064bca97c186820d90e
Fikir <fikirabc@gmail.com>
2025-08-09 10:02:15 +0300

    sticky

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -14,68 +14,70 @@ const Header = () => {
   const [{basket}, dispatch]=useContext(DataContext)
   console.log(basket.length)
     return (
-      <section>
-        <section className={Classes.header_container}>
-          <div className={Classes.logo_container}>
-            {/* logo */}
-            <Link to="/">
-              <img
-                src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
-                alt="log"
-              />
-            </Link>
-            {/* delivery */}
-            <span>
-              {/* icon */}
-              <MdOutlineLocationOn />
-            </span>
+      <section className={Classes.fixed}>
+        <section>
+          <section className={Classes.header_container}>
+            <div className={Classes.logo_container}>
+              {/* logo */}
+              <Link to="/">
+                <img
+                  src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                  alt="log"
+                />
+              </Link>
+              {/* delivery */}
+              <span>
+                {/* icon */}
+                <MdOutlineLocationOn />
+              </span>
 
-            <div className={Classes.delivery}>
-              <p>Delivery To</p>
-              <span>Ethiopia</span>
+              <div className={Classes.delivery}>
+                <p>Delivery To</p>
+                <span>Ethiopia</span>
+              </div>
             </div>
-          </div>
 
-          {/* search */}
-          <div className={Classes.search}>
-            <select>
-              <option value="">All </option>
-            </select>
-
-            <input type="text" placeholder="Amazon Search" />
-            <FaSearch size={25} />
-          </div>
-
-          <div className={Classes.order_container}>
-            <Link to="/" className={Classes.language}>
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_the_United_States.png/960px-Flag_of_the_United_States.png"
-                alt="Search"
-              />
+            {/* search */}
+            <div className={Classes.search}>
               <select>
-                <option>EN</option>
+                <option value="">All </option>
               </select>
-            </Link>
-            <Link to="/signin">
-              <div>
-                <p>Sign In</p>
-                <span>Account & List</span>
-              </div>
-            </Link>
-            <Link to="/order">
-              <div>
-                <p>Returnes</p>
-                <span>& Orders</span>
-              </div>
-            </Link>
 
-            <Link to="/cart" className={Classes.cart}>
-              <span>{basket.length}</span>
-              <BiCart />
-            </Link>
-          </div>
+              <input type="text" placeholder="Amazon Search" />
+              <FaSearch size={25} />
+            </div>
+
+            <div className={Classes.order_container}>
+              <Link to="/" className={Classes.language}>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Flag_of_the_United_States.png/960px-Flag_of_the_United_States.png"
+                  alt="Search"
+                />
+                <select>
+                  <option>EN</option>
+                </select>
+              </Link>
+              <Link to="/signin">
+                <div>
+                  <p>Sign In</p>
+                  <span>Account & List</span>
+                </div>
+              </Link>
+              <Link to="/order">
+                <div>
+                  <p>Returnes</p>
+                  <span>& Orders</span>
+                </div>
+              </Link>
+
+              <Link to="/cart" className={Classes.cart}>
+                <span>{basket.length}</span>
+                <BiCart />
+              </Link>
+            </div>
+          </section>
+          <LowerHeader />
         </section>
-        <LowerHeader />
       </section>
     );
 }
